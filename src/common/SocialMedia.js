206b82a5097b87af31f697f28a0fc6c73e1c5159
yamini <atul762@gmail.com>
2024-07-31 19:06:02 +0530

    header updated

--- src/common/SocialMedia.js
+++ src/common/SocialMedia.js
@@ -8,7 +8,7 @@ const SocialMedia = () => {
                 <a href="https://www.youtube.com/TennisIndiaLive" target="_blank" rel="noopener noreferrer" className="text-red-500">
                     <FaYoutube size={30} />
                 </a>
-                <a href="https://x.com/TennisOfIndia" target="_blank" rel="noopener noreferrer" className="text-blue-500">
+                <a href="https://x.com/TennisIndiaLive" target="_blank" rel="noopener noreferrer" className="text-blue-500">
                     <FaTwitter size={30} />
                 </a>
                 <a href="https://www.facebook.com/TheTennisOfIndia/" target="_blank" rel="noopener noreferrer" className="text-blue-700">
