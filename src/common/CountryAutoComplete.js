f3ec8dd2ea835684066b11c374c5a1b58c08bb1b
yamini <atul762@gmail.com>
2024-08-12 22:10:58 +0530

    release bracnch

--- src/common/CountryAutoComplete.js
+++ src/common/CountryAutoComplete.js
@@ -1,6 +1,7 @@
-// CountryAutocomplete.js
 import React from 'react';
 import { Autocomplete, TextField } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 import countries from 'i18n-iso-countries';
 import enLocale from 'i18n-iso-countries/langs/en.json';
 import { FlagIcon } from 'react-flag-kit';
@@ -8,48 +9,51 @@ import { FlagIcon } from 'react-flag-kit';
 countries.registerLocale(enLocale);
 
 const countryList = countries.getNames('en', { select: 'official' });
-const countryArray = Object.keys(countryList).map((key) => ({
-  code: key,
-  label: countryList[key],
-  name: countryList[key].toLowerCase()
-}));
+const alpha2ToAlpha3 = countries.getAlpha2Codes();
+console.log(countryList)
+const countryArray = Object.keys(countryList).map((key) => {
+  const label = countryList[key];
+  const abbreviatedLabel = key; // Use ISO 3166-1 alpha-3 country code as the abbreviation
+  return {
+    code: key,
+    label,
+    abbreviatedLabel,  // Three-letter code
+    name: label.toLowerCase(),
+    alpha3: alpha2ToAlpha3[key]
+  };
+});
 
 const CountryAutocomplete = ({ selectedCountry, handleCountryChange }) => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <Autocomplete
       options={countryArray}
-      getOptionLabel={(option) => option.label}
+      getOptionLabel={(option) => isSmallScreen ? option.abbreviatedLabel : option.label}
       value={countryArray.find((country) => country.name.toLowerCase() === selectedCountry.toLowerCase()) || null}
       onChange={(event, newValue) => {
         handleCountryChange(newValue ? newValue.name : '');
       }}
       renderOption={(props, option) => (
-        <li {...props} key={option.code}>
-          <FlagIcon code={option.code} size={16} style={{ marginRight: '8px' }} />
-          {option.label}
+        <li {...props} key={option.code} style={{ fontSize: isSmallScreen ? '12px' : '14px' }}>
+          <FlagIcon code={option.code} size={isSmallScreen ? 12 : 16} style={{ marginRight: isSmallScreen ? '4px' : '8px' }} />
+          {isSmallScreen ? option.alpha3 : option.label}
         </li>
       )}
-      renderInput={(params) => <TextField {...params} label="Country" variant="outlined" />}
-      size="small"
-      sx={{width:200}}
-      // sx={{
-      //   '& .MuiOutlinedInput-root': {
-      //     color: 'white',
-      //   },
-      //   '& .MuiInputLabel-root': {
-      //     color: 'white',
-      //   },
-      //   '& .MuiOutlinedInput-notchedOutline': {
-      //     borderColor: 'white',
-      //   },
-      //   '&:hover .MuiOutlinedInput-notchedOutline': {
-      //     borderColor: 'white',
-      //   },
-      //   '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-      //     borderColor: 'white',
-      //   },
-      //   width:200
-      // }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Country"
+          variant="outlined"
+          size="small"
+          InputProps={{
+            ...params.InputProps,
+            style: { fontSize: isSmallScreen ? '12px' : '14px' }
+          }}
+        />
+      )}
+      sx={{ width: isSmallScreen ? 150 : 200 }}
     />
   );
 };
--- src/common/DatePicker.js
+++ src/common/DatePicker.js
@@ -1,45 +1,62 @@
 import * as React from 'react';
-
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { useThemeProps } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import dayjs from 'dayjs';
 
-const CustomDatePicker = styled(DatePicker)({
-    '& .MuiInputBase-input': {
-      color: 'white',
-      padding: '6px 10px', //
-    },
-    '& .MuiInputLabel-root': {
-      color: 'white'
-    },
-    '& .MuiInputLabel-root.Mui-focused': {
-      color: 'white',
-    },
-    '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-      borderColor: 'white',
-    },
-    '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
-      borderColor: 'white',
-    },
-    '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
-      borderColor: 'white',
-    },
-  });
+const CustomDatePicker = styled(DatePicker)(({ theme }) => ({
+  '& .MuiInputBase-input': {
+    padding: '6px 10px',
+    [theme.breakpoints.down('sm')]: {
+      padding: '4px 8px', // Smaller padding on small screens
+    }
+  }
+  // '& .MuiInputLabel-root': {
+  //   color: 'white',
+  // },
+  // '& .MuiInputLabel-root.Mui-focused': {
+  //   color: 'white',
+  // },
+  // '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+  //   borderColor: 'white',
+  // },
+  // '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
+  //   borderColor: 'white',
+  // },
+  // '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+  //   borderColor: 'white',
+  // },
+}));
 
 export default function DatePickerValue(props) {
-  
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const isSmallScreen = windowWidth <= 600; // Adjust the breakpoint as needed
+
+  const formatDate = (date) => {
+    return isSmallScreen ? dayjs(date).format('MM/DD') : dayjs(date).format('MM/DD/YYYY');
+  };
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={['DatePicker', 'DatePicker']}>
-        <DatePicker
+        <CustomDatePicker
           label="Select Date"
           value={props.selectedDate}
           size="small"
           onChange={props.handleSelectDate}
+          inputFormat={formatDate(props.selectedDate)} // Apply the format based on screen size
         />
       </DemoContainer>
     </LocalizationProvider>
